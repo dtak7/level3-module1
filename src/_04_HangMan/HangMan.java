689e4c4448e7ba3ce325d4165ed89d7debd14ef3
GT <league@WTS14-4.attlocal.net>
2018-12-19 17:00:52 -0800

    hangman done

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,21 +1,28 @@
 package _04_HangMan;
 
+import java.awt.LayoutManager;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Stack;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class HangMan implements KeyListener {
 	Stack<String> words = new Stack<String>();
 	String currentWord;
 	String displayedWord = "";
+
 	int life = 10;
 	// Stack<Character> chars = new Stack<Character>();
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JLabel lives = new JLabel();
+	JLabel wrongAnswers = new JLabel();
 
 	public static void main(String[] args) {
 		HangMan manhang = new HangMan();
@@ -24,7 +31,6 @@ public class HangMan implements KeyListener {
 		manhang.setWord();
 		manhang.createFrame();
 		manhang.addListener();
-		
 
 	}
 
@@ -39,22 +45,29 @@ public class HangMan implements KeyListener {
 
 	void setWord() {
 		currentWord = words.pop();
-		displayedWord="";
+		displayedWord = "";
 		for (int i = 0; i < currentWord.length(); i++) {
 			displayedWord += "_";
 		}
 	}
+
 	void addListener() {
 		frame.addKeyListener(this);
 	}
 
 	void createFrame() {
+		frame.add(panel);
+		panel.add(lives);
+		panel.add(label);
 		label.setText(displayedWord);
-
-		frame.add(label);
+		lives.setText("      you have " + life + " lives");
+		wrongAnswers.setText("you have incorrectly guessed: ");
 		frame.setVisible(true);
 		frame.setSize(300, 300);
-		
+
+		panel.setLayout((LayoutManager) new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(lives);
+		panel.add(wrongAnswers);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
@@ -67,6 +80,7 @@ public class HangMan implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+
 		// TODO Auto-generated method stub
 
 		// String finalText = label.getText() + e.getKeyChar();
@@ -86,19 +100,26 @@ public class HangMan implements KeyListener {
 
 			else {
 				newWord += displayedWord.charAt(i);
+				// wrongAnswers.setText("you have incorrectly guessed:" + d );
 
 			}
 
 		}
 		displayedWord = newWord;
 		label.setText(displayedWord);
+		if (currentWord.equals(displayedWord)) {
+			JOptionPane.showMessageDialog(null, "YOU WIN. Congrats");
+		}
 		if (!isCorrect) {
 			life -= 1;
-			System.out.println("you have " + life + " lives");
+			lives.setText("      you have " + life + " lives");
+
 		}
+
 		if (life == 0) {
 			String playAgain = JOptionPane.showInputDialog(
 					"Game Over. The correct answer was " + currentWord + " would you like to play again?");
+
 			if (playAgain.equals("yes")) {
 				System.out.println("working!");
 				createStack();
@@ -106,17 +127,19 @@ public class HangMan implements KeyListener {
 				frame.remove(label);
 				frame.dispose();
 				createFrame();
-				
+
 				life = 10;
+				lives.setText("      you have " + life + " lives");
 			}
 
 			else {
 				JOptionPane.showMessageDialog(null, "Thanks for playing!");
+
+				frame.dispose();
 			}
-			if(currentWord==displayedWord) {
-				System.out.println("you win");
-			}
+
 		}
+
 	}
 
 	@Override
