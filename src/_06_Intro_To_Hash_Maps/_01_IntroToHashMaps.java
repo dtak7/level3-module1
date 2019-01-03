c649395eedbe32cb5cdd7b416afe618c7bf6c4f8
GT <league@WTS14-4.attlocal.net>
2019-01-02 18:29:43 -0800

    intro to hash maps, log search ip

--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -4,17 +4,27 @@ import java.util.HashMap;
 
 public class _01_IntroToHashMaps {
 	public static void main(String[] args) {
-		//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
+		// 1. Create a HashMap called roots with Integers for the keys and Doubles for
+		// the values.
+		HashMap<Integer, Double> hash = new <Integer, Double>HashMap();
+		// 2. Using a for-loop, add 500 entries to your HashMap. The key entry will be
+		// the
+		// current iteration of the loop (i). The value entry will be the square root of
+		// i.
+		for (int i = 0; i < 501; i++) {
+			hash.put(i, Math.sqrt(i));
+		}
+		// 3. Iterate through all the entries in your HashMap displaying the keys with
+		// their respective
+		// square roots (values). Use the following format.
+		for (Integer i : hash.keySet()) {
+			System.out.println("The square root of " + i + " is " + hash.get(i));
+		}
 
-		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
-		//   current iteration of the loop (i). The value entry will be the square root of i.
+		// The square Root of 0 is 0.0
+		// The square Root of 1 is 1.0
+		// The square Root of 2 is 1.4142135623730951
+		// The square Root of 3 is 1.7320508075688772
 
-		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
-		//   square roots (values). Use the following format.
-
-		//   The square Root of 0 is 0.0
-		//   The square Root of 1 is 1.0
-		//   The square Root of 2 is 1.4142135623730951
-		//   The square Root of 3 is 1.7320508075688772
 	}
 }
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,33 +1,79 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
-  /* 
-	 * Crate a HashMap of Integers for the keys and Strings for the values.
-	 * Create a GUI with three buttons. 
-	 * Button 1: Add Entry
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				After an ID is entered, use another input dialog to ask the user to enter a name.
-	 * 				Add this information as a new entry to your HashMap.
-	 * 
-	 * Button 2: Search by ID
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				If that ID exists, display that name to the user.
-	 * 				Otherwise, tell the user that that entry does not exist.
-	 * 
-	 * Button 3: View List
-	 * 				When this button is clicked, display the entire list in a message dialog in the following format:
-	 * 				ID: 123  Name: Harry Howard
-	 * 				ID: 245  Name: Polly Powers
-	 * 				ID: 433  Name: Oliver Ortega
-	 * 				etc...
-	 * 
-	 * When this is complete, add a fourth button to your window.
-	 * Button 4: Remove Entry
-	 * 				When this button is clicked, prompt the user to enter an ID using an input dialog.
-	 * 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
-	 * 				is not in the list. 
-	 *
-	 * */
-	
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
+
+	// Crate a HashMap of Integers for the keys and Strings for the values.
+	HashMap<Integer, String> hash = new <Integer, String>HashMap();
+
+	public static void main(String[] args) {
+		_02_LogSearch logSearch = new _02_LogSearch();
+		logSearch.createGUI();
+	}
+
+	// / * Create a GUI with three buttons.
+	void createGUI() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton entry = new JButton();
+		JButton search = new JButton();
+		JButton view = new JButton();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		panel.add(entry);
+		panel.add(search);
+		panel.add(view);
+		entry.setText("entry");
+		entry.addActionListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	// * Button 1: Add Entry
+	// * When this button is clicked, use an input dialog to ask the user to enter
+	// an ID number.
+	// * After an ID is entered, use another input dialog to ask the user to enter a
+	// name.
+	// * Add this information as a new entry to your HashMap.
+	// *
+	// * Button 2: Search by ID
+	// * When this button is clicked, use an input dialog to ask the user to enter
+	// an ID number.
+	// * If that ID exists, display that name to the user.
+	// * Otherwise, tell the user that that entry does not exist.
+	// *
+	// * Button 3: View List
+	// * When this button is clicked, display the entire list in a message dialog in
+	// the following format:
+	// * ID: 123 Name: Harry Howard
+	/// * ID: 245 Name: Polly Powers
+	// * ID: 433 Name: Oliver Ortega
+	// * etc...
+	// *
+	// * When this is complete, add a fourth button to your window.
+	// * Button 4: Remove Entry
+	// * When this button is clicked, prompt the user to enter an ID using an input
+	// dialog.
+	// * If this ID exists in the HashMap, remove it. Otherwise, notify the user
+	// that the ID
+	// * is not in the list.
+	// *
+	// * */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String ID = JOptionPane.showInputDialog(null, "enter and ID NUMBER");
+		int result = Integer.parseInt(ID);
+		String name = JOptionPane.showInputDialog(null, "enter a name");
+		hash.put(result, name);
+	}
+
 }
